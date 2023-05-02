6980a38fe8affe08107b3c8308db23014e4f9178
Alan Cruz <acruzdelangel887@gmail.com>
2023-05-01 23:09:07 -0600

    correcion errores, seccion editar

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express=require('express');
 const morgan=require('morgan');
 const mongoose=require('mongoose');
-const Celular=require('./Celular');
-const cliente=require('./cliente');
-const Vendedor = require('./Vendedor');
+const celular = require('./Schemas/Celular');
+const cliente = require('./Schemas/Cliente')
+const vendedor = require('./Schemas/Vendedor')
 const app=express();
 
-app.set('port',process.env.PORT||3600);
+app.set('port',process.env.PORT||3200);
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:false}));
 
@@ -20,30 +20,30 @@ mongoose.connect("mongodb+srv://Alan:HailedSquare881@alan.rbrdfgi.mongodb.net/ve
 
 //Consulta general celular
 app.get('/celulares', async(req,res)=>{
-    const celulares = await cliente.find();
-    res.render('celular',{celulares})
+    const celulares = await celular.find();
+    res.render('index',{celulares})
 })
 
 //Insertar celular
 app.post('/insertarCelular', async(req,res)=>{
     const celularInsertardo = new celular(req.body);
     await celularInsertardo.save();
-    res.redirect("/celular")
+    res.redirect("/celulares")
 })
 //Eliminar celular
 app.get('/eliminarCelular/:id', async(req,res)=>{
     await celular.findOneAndDelete({codigo:req.params.id});
-    res.redirect("/celular")
+    res.redirect("/celulares")
 })
 
 //Actualizar celular
 app.post('/actualizarCelular/:id', async(req,res)=>{
     await celular.findOneAndUpdate({codigo:req.params.id},req.body);
-    res.redirect("/celular")
+    res.redirect("/celulares")
 })
 //Consulta individual celular
 app.get('/consultaCelular/:id', async(req,res)=>{
-    const celular1 = await Celular.findOne({codigo:req.params.id});
+    const celular1 = await celular.findOne({codigo:req.params.id});
     res.render('editarCelular',{celular1}) 
 })
 
